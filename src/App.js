b9830af784d9c24a92ca2a7673f712a6205354a8
Janki <jnkdesignhouse@gmail.com>
2018-10-23 15:38:24 -0600

    prettified JS, no real programming because React is scary

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import React from 'react'
 import './App.css'
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
+    state = {
+        /**
+         * TODO: Instead of using this state variable to keep track of which page
+         * we're on, use the URL in the browser's address bar. This will ensure that
+         * users can use the browser's back and forward buttons to navigate between
+         * pages, as well as provide a good URL they can bookmark and share.
+         */
+        showSearchPage: false
+    }
 
-  render() {
-    return (
-      <div className="app">
+    render() {
+        return (
+            <div className="app">
         {this.state.showSearchPage ? (
           <div className="search-books">
             <div className="search-books-bar">
@@ -199,8 +199,8 @@ class BooksApp extends React.Component {
           </div>
         )}
       </div>
-    )
-  }
+        )
+    }
 }
 
 export default BooksApp

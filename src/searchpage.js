d0f548f3ccb4966a203941f47a41db61fb555c99
Janki <jnkdesignhouse@gmail.com>
2018-11-05 03:32:23 -0700

    removed unnecessary stuff

--- src/searchpage.js
+++ src/searchpage.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import escapeRegExp from 'escape-string-regexp'
-import sortBy from 'sort-by'
 
 class SearchPage extends Component {
     static propTypes = {
